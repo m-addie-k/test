9cba08071bcb5529a2d124d2599ff2f16678b48a
m-addie-k <kmtestsite0609@gmail.com>
2019-04-01 12:26:21 +0300

    Update IndexController.php

--- src/IndexController.php
+++ src/IndexController.php
@@ -1,18 +1,18 @@
 <?php
-use Phalcon\Http\Response;
-use Base\Components\UserComponent;
-use Base\Components\ConfigComponent;
+// use Phalcon\Http\Response;
+// use Base\Components\UserComponent;
+// use Base\Components\ConfigComponent;
 
 class IndexController extends ControllerBase {
     public function indexAction(){
 
-        $users = new UserComponent();
+//         $users = new UserComponent();
         // throw new \Exception("Error Processing Request", 1);
-        $users->save();
-        var_dump($users->getMessages());
+//         $users->save();
+//         var_dump($users->getMessages());
 
-        $gc = new ConfigComponent();
-        var_dump($gc->getValue("log-file-path"));
+//         $gc = new ConfigComponent();
+//         var_dump($gc->getValue("log-file-path"));
         echo "lol";
         $this->view->disable();
     }
